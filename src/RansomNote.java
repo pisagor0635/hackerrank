1c6559854151b6b5111a59f6c38556c431d50fd5
Adnan Bilgen <adnan_bilgen@hotmail.com>
2021-03-29 01:42:21 +0300

    RansomNote - Solution

---
+++ src/RansomNote.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class RansomNote {
+
+    static void checkMagazine(String[] magazine, String[] note) {
+
+        Map<String, Long> wordsMagazine = Arrays.stream(magazine).collect(groupingBy(Function.identity(), counting()));
+
+        Map<String, Long> wordsNote = Arrays.stream(note).collect(groupingBy(Function.identity(), counting()));
+
+        Set<String> words = wordsNote.keySet();
+
+        for (String s : words) {
+            if(wordsMagazine.containsKey(s)){
+                if (wordsNote.get(s) > wordsMagazine.get(s)) {
+                    System.out.println("No");
+                    return;
+                }
+            }else{
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
