cc8690ae27eb6ac94ca1266990d6e1c5b65b776b
Adnan Bilgen <adnan_bilgen@hotmail.com>
2021-03-29 01:42:55 +0300

    RansomNote - Solution

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -9,13 +9,9 @@ import static java.util.stream.Collectors.groupingBy;
 public class RansomNote {
 
     static void checkMagazine(String[] magazine, String[] note) {
-
         Map<String, Long> wordsMagazine = Arrays.stream(magazine).collect(groupingBy(Function.identity(), counting()));
-
         Map<String, Long> wordsNote = Arrays.stream(note).collect(groupingBy(Function.identity(), counting()));
-
         Set<String> words = wordsNote.keySet();
-
         for (String s : words) {
             if(wordsMagazine.containsKey(s)){
                 if (wordsNote.get(s) > wordsMagazine.get(s)) {
