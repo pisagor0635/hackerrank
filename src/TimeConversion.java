f40006bf80d89e43fbc6880076d3c36bcc11d5b3
Adnan Bilgen <adnan_bilgen@hotmail.com>
2021-05-18 02:01:24 +0300

    Change the data type from String to String Builder

--- src/TimeConversion.java
+++ src/TimeConversion.java
@@ -1,34 +1,27 @@
 public class TimeConversion {
 
     public static void main(String[] args) {
-
-        String time = "12:05:45AM";
+        String time = "1:05:45PM";
         System.out.println(timeConversion(time));
-
     }
 
     public static String timeConversion(String s) {
-
-        String converted = "";
+        StringBuilder converted = new StringBuilder();
         String key = s.substring(s.length() - 2);
         String[] values = s.substring(0, s.length() - 2).split(":");
         if (key.equals("PM")) {
             if (values[0].equals("12")) {
-                converted = values[0] + ":" + values[1] + ":" + values[2];
+                converted.append(values[0]).append(":").append(values[1]).append(":").append(values[2]);
             } else {
-                converted = (Integer.parseInt(values[0]) + 12) + ":" + values[1] + ":" + values[2];
+                converted.append(Integer.parseInt(values[0]) + 12).append(":").append(values[1]).append(":").append(values[2]);
             }
         } else if (key.equals("AM")) {
             if (values[0].equals("12")) {
-                converted = "00" + ":" + values[1] + ":" + values[2];
+                converted.append("00").append(":").append(values[1]).append(":").append(values[2]);
             } else {
-                converted = values[0] + ":" + values[1] + ":" + values[2];
+                converted.append(values[0]).append(":").append(values[1]).append(":").append(values[2]);
             }
         }
-
-        return converted;
-
-
+        return converted.toString();
     }
-
 }
